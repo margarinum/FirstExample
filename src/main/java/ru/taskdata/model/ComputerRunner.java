1625ba36364ba934eeb468da48c6c4b1912e924a
Дмитрий <margarinum@gmail.com>
2018-09-24 19:55:18 +0300

    FirstExample

--- src/main/java/ru/taskdata/model/ComputerRunner.java
+++ src/main/java/ru/taskdata/model/ComputerRunner.java
@@ -2,14 +2,22 @@ package ru.taskdata.model;
 
 import ru.taskdata.model.impl.Computer;
 
-public class ComputerRunner {
+import java.util.HashMap;
+import java.util.Map;
 
-    IComputer object;
+public class ComputerRunner  {
 
-    public Computer add(){
 
-        return new Computer();
+    Map<IComputer, String> hashmap = new HashMap<IComputer, String>();
 
+    public Map<IComputer, String> add(IComputer computer_obj){
+
+        hashmap.put(computer_obj, getAddr(computer_obj));
+        return hashmap;
+    }
+
+    private String getAddr(IComputer obj){
+       return obj.toString();
     }
 
     public void run(IComputer computer){
